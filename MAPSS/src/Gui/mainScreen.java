78c9a1029301f5debff72db566bc31d8d05aca31
jaaplallie <jaaplallie@gmail.com>
2013-10-03 11:29:29 +0200

    sync

--- MAPSS/src/Gui/mainScreen.java
+++ MAPSS/src/Gui/mainScreen.java
@@ -1,5 +1,18 @@
 package Gui;
 
-public class mainScreen {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class mainScreen extends JFrame implements ActionListener {
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
